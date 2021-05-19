09621fb984e86efb10ea01518e10a7c1997804a4
shaman1979 <upgamestudio@gmail.com>
2021-05-19 22:39:14 +0300

    Добавил тест для проверки очищения прогресса

--- Slider/Assets/Scripts/DebugMode/ProgressDataReceiver.cs
+++ Slider/Assets/Scripts/DebugMode/ProgressDataReceiver.cs
@@ -1,6 +1,4 @@
 ﻿using Slicer.Application.Storages;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Slicer.DebugMode
---
+++ Slider/Assets/Tests/Game/ProgressDataTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Slicer.Application.Storages;
+using Slicer.DebugMode;
+using UnityEngine;
+
+namespace Tests.Game
+{
+    public class ProgressDataTest
+    {
+        [Test]
+        public void WhenRemoveProgress_ThenProgressDataClear()
+        {
+            //Act
+            ProgressDataReceiver.RemoveProgress();
+            
+            //Assert
+            Assert.AreEqual(0, PlayerPrefs.GetInt(PlayerPrefsKeyStorage.XP));
+            Assert.AreEqual(0, PlayerPrefs.GetInt(PlayerPrefsKeyStorage.LEVEL));
+            Assert.AreEqual(0, PlayerPrefs.GetInt(PlayerPrefsKeyStorage.STARS));
+        }
+    }
+}
